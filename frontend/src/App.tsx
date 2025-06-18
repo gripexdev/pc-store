ffe7fcb2c9f56e4bb86a98f4977cd1f72a7d33af
gripexdev <cj.othman@gmail.com>
2025-06-18 18:02:53 +0100

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+      Hello from Tailwind!
+    </div>
+  );
+}
+
+
+export default App;
