0bd3ddaf7c1f6ba0cefb312f18ca5f4fa5ee21f7
gripexdev <cj.othman@gmail.com>
2025-06-23 00:12:05 +0100

    Add .env.example with required environment variables

--- backend/src/utils/cloudinary.ts
+++ backend/src/utils/cloudinary.ts
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'dywfxoze6',
-  api_key: '813373448179351',
-  api_secret: '_mKM53XQkM085MHEfV68ANfyV7U',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dywfxoze6',
+  api_key: process.env.CLOUDINARY_API_KEY || '813373448179351',
+  api_secret: process.env.CLOUDINARY_API_SECRET || '_mKM53XQkM085MHEfV68ANfyV7U',
   secure: true,
 });
 
